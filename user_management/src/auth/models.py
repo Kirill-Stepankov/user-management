c0810f79ca0911b99d4f247c7900d3e988829439
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-10-10 13:11:28 +0300

    update db models: String -> Text

--- user_management/src/auth/models.py
+++ user_management/src/auth/models.py
@@ -2,7 +2,7 @@ import enum
 import uuid
 from datetime import datetime
 
-from sqlalchemy import UUID, Boolean, DateTime, ForeignKey, String
+from sqlalchemy import UUID, Boolean, DateTime, ForeignKey, Text
 from sqlalchemy.orm import Mapped, mapped_column
 from src.database import Base
 
@@ -17,7 +17,7 @@ class Group(Base):
     __tablename__ = "group"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(250), unique=True, index=True)
+    name: Mapped[str] = mapped_column(Text, unique=True, index=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime, default=datetime.utcnow, index=True
     )
@@ -27,11 +27,11 @@ class User(Base):
     __tablename__ = "user"
 
     uuid: Mapped[uuid] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
-    name: Mapped[str] = mapped_column(String(250))
-    surname: Mapped[str] = mapped_column(String(250))
-    username: Mapped[str] = mapped_column(String(250), unique=True, index=True)
-    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
-    phone_number: Mapped[str] = mapped_column(String(30))
+    name: Mapped[str] = mapped_column(Text)
+    surname: Mapped[str] = mapped_column(Text)
+    username: Mapped[str] = mapped_column(Text, unique=True, index=True)
+    email: Mapped[str] = mapped_column(Text, unique=True, index=True)
+    phone_number: Mapped[str] = mapped_column(Text)
     role: Mapped[Role]
     group_id: Mapped[int] = mapped_column(ForeignKey("group.id"))
     is_blocked: Mapped[bool] = mapped_column(Boolean, default=True)
@@ -41,4 +41,4 @@ class User(Base):
     updated_at: Mapped[datetime] = mapped_column(
         DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
     )
-    s3_path: Mapped[str] = mapped_column(String(2083))
+    s3_path: Mapped[str] = mapped_column(Text)
