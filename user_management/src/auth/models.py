9f3f5a1b0e46313cc6ad1a6b79c24a4708ec44c3
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-10-10 11:33:45 +0300

    database models: user, group

---
+++ user_management/src/auth/models.py
@@ -0,0 +1,44 @@
+import enum
+import uuid
+from datetime import datetime
+
+from sqlalchemy import UUID, Boolean, DateTime, ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column
+from src.database import Base
+
+
+class Role(enum.Enum):
+    USER = "User"
+    ADMIN = "Admin"
+    MODERATOR = "Moderator"
+
+
+class Group(Base):
+    __tablename__ = "group"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, index=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow, index=True
+    )
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    uuid: Mapped[uuid] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(String(250))
+    surname: Mapped[str] = mapped_column(String(250))
+    username: Mapped[str] = mapped_column(String(250), unique=True, index=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    phone_number: Mapped[str] = mapped_column(String(30))
+    role: Mapped[Role]
+    group_id: Mapped[int] = mapped_column(ForeignKey("group.id"))
+    is_blocked: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow, index=True
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+    s3_path: Mapped[str] = mapped_column(String(2083))
---
+++ user_management/src/database.py
@@ -0,0 +1,23 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+from .config import get_settings
+
+settings = get_settings()
+
+
+DATABASE_URL = f"postgresql+asyncpg://{settings.postgres_user}:{settings.postgres_password}@{settings.postgres_host}:{settings.postgres_port}/{settings.postgres_db}"
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
