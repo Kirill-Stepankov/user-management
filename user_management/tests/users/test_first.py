3294ad6553b3ee513ba7ae7d6b178c82922f5975
Kirill-Stepankov <kirillstepankov18@gmail.com>
2023-10-23 15:51:49 +0300

    pytest initial setup

--- user_management/tests/users/test_first.py
+++ user_management/tests/users/test_first.py
@@ -1,2 +1,18 @@
 def test_a():
     assert 1 == 1
+
+
+from sqlalchemy import insert, select
+from src.users.models import Group
+from tests.conftest import async_session_maker
+
+
+async def test_add_role():
+    async with async_session_maker() as session:
+        stmt = insert(Group).values(name="wow")
+        await session.execute(stmt)
+        await session.commit()
+
+        query = select(Group)
+        result = await session.execute(query)
+        assert result.all()[0][0].name == "wow"
